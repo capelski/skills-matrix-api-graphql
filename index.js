712ea3575e25ae82a697bec271dba8432b07863d
Capellas Careta <carles.capellas@sage.com>
2018-07-06 13:10:26 +0200

    Extract middleware functions into controllers

--- index.js
+++ index.js
@@ -1,72 +1,22 @@
 const { express } = require('modena');
 var router = express.Router();
-var employeeService = require('./services/employee-service');
-var skillService = require('./services/skill-service');
+var employeesController = require('./controllers/employees-controller');
+var skillsContoller = require('./controllers/skills-controller');
 
 const configureRouter = (middleware) => {
-	router.get('/api/employee', function (req, res, next) {
-		const keywords = req.query.keywords;
-		const page = parseInt(req.query.page) || 0;
-		const pageSize = parseInt(req.query.pageSize) || 10;
-		const employees = employeeService.getAll(keywords, page, pageSize);
-		return res.json(employees);
-	});
-	router.get('/api/employee/getById', function (req, res, next) {
-		const id = req.query.id;
-		const employee = employeeService.getById(id);
-		return res.json(employee);
-	});
-	router.get('/api/employee/getMostSkilled', function (req, res, next) {
-		const mostSkilledEmployees = employeeService.getMostSkilled();
-		return res.json(mostSkilledEmployees);
-	});
-	router.post('/api/employee', [middleware.bodyParser, function (req, res, next) {
-		var employee = req.body;
-		employee = employeeService.create(employee);
-		return res.json(employee);
-	}]);
-	router.put('/api/employee', [middleware.bodyParser, function (req, res, next) {
-		var employeeData = req.body;
-		var employee = employeeService.update(employeeData);
-		return res.json(employee);
-	}]);
-	router.delete('/api/employee', function (req, res, next) {
-		const id = req.query.id;
-		var employee = employeeService.deleteEmployee(id);
-		return res.json(employee);
-	});
+	router.get('/api/employee', employeesController.getAll);
+	router.get('/api/employee/getById', employeesController.getById);
+	router.get('/api/employee/getMostSkilled', employeesController.getMostSkilled);
+	router.post('/api/employee', [middleware.bodyParser, employeesController.create]);
+	router.put('/api/employee', [middleware.bodyParser, employeesController.update]);
+	router.delete('/api/employee', employeesController.deleteEmployee);
 
-	router.get('/api/skill', function (req, res, next) {
-		const keywords = req.query.keywords;
-		const page = parseInt(req.query.page) || 0;
-		const pageSize = parseInt(req.query.pageSize) || 10;
-		const skills = skillService.getAll(keywords, page, pageSize);
-		return res.json(skills);
-	});
-	router.get('/api/skill/getById', function (req, res, next) {
-		const id = req.query.id;
-		const skill = skillService.getById(id);
-		return res.json(skill);
-	});
-	router.get('/api/skill/getRearest', function (req, res, next) {
-		const rearestSkills = skillService.getRearest();
-		return res.json(rearestSkills);
-	});
-	router.post('/api/skill', [middleware.bodyParser, function (req, res, next) {
-		var skill = req.body;
-		skill = skillService.create(skill);
-		return res.json(skill);
-	}]);
-	router.put('/api/skill', [middleware.bodyParser, function (req, res, next) {
-		var skillData = req.body;
-		var skill = skillService.update(skillData);
-		return res.json(skill);
-	}]);
-	router.delete('/api/skill', function (req, res, next) {
-		const id = req.query.id;
-		var skill = skillService.deleteSkill(id);
-		return res.json(skill);
-	});
+	router.get('/api/skill', skillsContoller.getAll);
+	router.get('/api/skill/getById', skillsContoller.getById);
+	router.get('/api/skill/getRearest', skillsContoller.getRearest);
+	router.post('/api/skill', [middleware.bodyParser, skillsContoller.create]);
+	router.put('/api/skill', [middleware.bodyParser, skillsContoller.update]);
+	router.delete('/api/skill', skillsContoller.deleteSkill);
 
 	return router;
 }
