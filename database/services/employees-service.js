6ae0943e1e61429c407623ae305909b2eec81ff5
Carles Capellas Careta <capellas.carles@gmail.com>
2018-07-29 12:03:42 +0200

    Database employees service WIP

--- database/services/employees-service.js
+++ database/services/employees-service.js
@@ -1,8 +1,13 @@
 const employeesService = models => {
 
-	// const create = employee => {
-	// 	return models.Employee.create(employee);
-	// };
+	const create = employee => {
+		return models.Employee.create(employee);
+	};
+
+	const deleteEmployee = id => {
+		return models.Employee.find(id)
+		.then(employee => employee.destroy());
+	};
 
 	// return {
 	// 	create,
