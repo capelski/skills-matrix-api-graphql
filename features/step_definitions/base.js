4e24107ce6a291ab662be170a9eeab7ca1c9c5f2
Carles Capellas <carles.capellas@sage.com>
2019-10-28 11:28:47 +0100

    Add support for sql sentences testing using alasql

--- features/step_definitions/base.js
+++ features/step_definitions/base.js
@@ -1,6 +1,7 @@
 const { Given, When } = require('cucumber');
 const { graphql } = require('graphql');
 const shared = require('./shared');
+const alasql = require('alasql');
 
 Given('the defined GraphQL schema', () => {
     shared.schema = require('../../schema');
@@ -8,6 +9,52 @@ Given('the defined GraphQL schema', () => {
 
 Given('the in-memory repositories', () => {
     const repositories = require('../../repositories/in-memory')();
+    shared.context = require('../../context')(repositories);
+});
+
+Given('the postgre repositories', () => {
+    const alasqlClient = {
+        query: (sql, parameters) => {
+            // Alasql doesn't support parametrized queries; we need to replace the values
+            sql = parameters.reduce((reduced, next, index) => {
+                return reduced.replace(`$${index + 1}`, typeof next === 'string' ? `'${next}'` : next);
+            }, sql);
+
+            // We replace the actual database tables with json files
+            sql = sql.replace('JOIN employee_skill', 'JOIN json("./repositories/postgre/alasql/employees-skills") as employee_skill')
+                .replace('FROM skill', 'FROM json("./repositories/postgre/alasql/skills") as skill')
+                .replace('FROM employee', 'FROM json("./repositories/postgre/alasql/employees") as employee');
+
+            // Alasql returns an incorrect data set when ordering by a COUNT() if the COUNT()
+            // is not selected
+            const hasOrderByCount = sql.match(/ORDER BY COUNT\((.*)\)/);
+            if (hasOrderByCount) {
+                sql = sql.replace(/SELECT (.*) FROM/, `SELECT $1, COUNT(${hasOrderByCount[1]}) FROM`);
+            }
+
+            return alasql.promise(sql)
+                .then(rows => {
+                    // console.log(sql);
+                    // console.log(rows);
+                    if (rows[0] && rows[0]['COUNT(*)']) {
+                        return {
+                            rows: [{
+                                count: rows[0]['COUNT(*)']
+                            }]
+                        };
+                    }
+                    return {
+                        rows
+                    };
+                })
+                .catch((error) => {
+                    console.log(sql);
+                    console.error(error);
+                    throw error;
+                });
+        }
+    };
+    const repositories = require('../../repositories/postgre')(alasqlClient);
     shared.context = require('../../context')(repositories);
 });
 
